e1ef52447d30f6e49e9ded469001f17dbf8c1c20
Paul Peavyhouse <pv@swooby.com>
2019-06-15 03:13:35 -0700

    Fairly nicely working!

--- main.js
+++ main.js
@@ -2,19 +2,60 @@
 function main() {
   //console.log('+main()');
 
-  var result;
+  // <section data-released="0" class="Release__Wrapper-sc-1y9ya50-0 kzsHGd">...</section>
+  var unreleaseds = $('section.Release__Wrapper-sc-1y9ya50-0.kzsHGd');
+  //console.log('unreleaseds', unreleaseds);
+  //unreleaseds.css('border', '3px solid red');
+  unreleaseds.each((index, unreleased) => {
+    //console.log('unreleased', unreleased);
+    unreleased = $(unreleased);
+    var cardCounts = unreleased.find($('h3.Category__CardCount-sc-3z36kz-4.cgDnlq')); // "1 entry"
+    //console.log('cardCounts', cardCounts);
+    var totalCount = 0;
+    var totalCountCompleted = 0;
+    cardCounts.each((index, cardCount) => {
+      cardCount = $(cardCount);
+      var cardCountText = cardCount.text();
+      //console.log('cardCountText1', cardCountText);
+      var taskCount = cardCount.find($('span.Category__TaskCount-sc-3z36kz-5.grmSrF')); // "<span class="Category__CountSeparator-sc-3z36kz-6 gaLoHd"></span>9 completed, 4 in development"
+      //console.log('taskCount', taskCount);
+      var taskCountText = taskCount.text();
+      //console.log(`taskCountText="${taskCountText}"`);
+      cardCountText = cardCountText.replace(taskCountText, '');
+      //console.log('cardCountText2', cardCountText);
+      var cardCountTotal = parseInt(cardCountText.split(' ')[0]);
+      //console.log('cardCountTotal', cardCountTotal);
 
-  // <div class="Release__Categories-sc-1y9ya50-6 biwapj">...</div>
-  result = $('div.Release__Categories-sc-1y9ya50-6.biwapj');
-  //console.log('result', result);
-  //result.css('border', '3px solid red');
+      var taskCountParts = taskCountText.split(', ');
+      //console.log('taskCountParts', taskCountParts);
+      var taskCountCompleted = 0;
+      if (taskCountParts.length > 0) {
+        taskCountParts.forEach(taskCountPart => {
+          //console.log(`taskCountPart="${taskCountPart}"`);
+          if (taskCountPart === '') {
+            return;
+          }
+          var index = taskCountPart.indexOf(' ');
+          var partValue = parseInt(taskCountPart.substring(0, index));
+          //console.log(`partValue="${partValue}"`);
+          var partName = taskCountPart.substring(index).toLowerCase().trim();
+          //console.log(`partName="${partName}"`);
+          if (partName === 'completed') {
+            taskCountCompleted = partValue;
+          }
+        });
+      }
+      //console.log('taskCountCompleted', taskCountCompleted);
 
-  // <h3 class="Release__Description-sc-1y9ya50-4 jSxVzn">Q3 2019</h3>
-  result = $('h3.Release__Description-sc-1y9ya50-4.jSxVzn');
-  //console.log('result', result);
-  //result.css('border', '3px solid red');
-  result.hide(); 
-  result.html('X% (Y of Z)'); // TODO:(pv)
+      totalCount += cardCountTotal;
+      totalCountCompleted += taskCountCompleted;
+    });
+    //console.log('totalCount', totalCount);
+    //console.log('totalCountCompleted', totalCountCompleted);
+    var subtitle = unreleased.find($('h3.Release__Description-sc-1y9ya50-4.jSxVzn'));
+    //console.log('subtitle', subtitle);
+    subtitle.html(`${parseFloat(100 * totalCountCompleted / totalCount).toFixed(2)}% (${totalCountCompleted} completed of ${totalCount})`)
+  });
   
   //console.log('-main()');
 }
